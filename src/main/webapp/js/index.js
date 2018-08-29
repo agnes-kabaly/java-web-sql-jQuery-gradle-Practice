9182eecbc51fdd876ed86cb2900294b211134830
agnes-kabaly <alicet.woods@gmail.com>
2018-08-29 23:55:56 +0200

    displayAllFoods jQuery method

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,3 +1,21 @@
 /**
  * Created by kabaly on 8/29/18.
  */
+$(document).ready(function () {
+    displayAllFoods();
+});
+
+function displayAllFoods() {
+    $.get("/allFood", function (response) {
+        $("#displayFoods").empty();
+        appendDisplayFoods(response);
+    });
+};
+
+function appendDisplayFoods(response) {
+    response.forEach(function (element) {
+        var foodH = document.createElement("h3");
+        foodH.textContent = element.id + ", " + element.name + ", " + element.calories + ", " + element.groupId;
+        $("#displayFoods").append(foodH);
+    });
+}
